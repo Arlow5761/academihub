44669dced6efe6edaf68d6bdd236aa90307ff20f
KarRz <reza11781@gmail.com>
2024-08-24 22:33:35 +0700

    feat: add Navbar

---
+++ app/components/HeadHomepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeadHomepage = () => {
+  return (
+    <div>
+    <h2 className="text-[#1F3C88] text-center text-3xl font-bold mt-8 px-4">Lagi cari informasi lomba, beasiswa, atau seminar?</h2>
+    <h2 className="text-[#1F3C88] text-center text-3xl font-bold mt-4 mb-4 px-4">
+        <span className="text-[#EE6F57]">AcademiHub</span> aja!</h2>
+        <hr style={{ borderTop: "2px solid #1F3C88", borderRadius: "0.5rem", margin: "0 1rem" }} />
+    <h2 className=" text-center text-4xl font-bold text-[#1F3C88] my-4 px-4">Cari apa?</h2>
+    </div>
+  )
+}
+
+export default HeadHomepage
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import CardSection from "./components/CardSection";
+import HeadHomepage from "./components/HeadHomepage";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      Hello World!
+    <main className="flex min-h-screen flex-col bg-[#F6F5F5]">
+      <Navbar/>
+      <div className="container mx-auto px-12 py-12">
+        <HeadHomepage/>
+        <CardSection/>
+      </div>
     </main>
   );
 }
