1e77f9cb47ae908a867b73a69789d06c2866f9b1
Arlow5761 <arlow5761@gmail.com>
2024-08-24 09:37:01 +0700

    feat: add register api endpoint and function

---
+++ app/api/user/register/route.ts
@@ -0,0 +1,19 @@
+import RegisterUser from "@/app/lib/register";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function POST(request: NextRequest) {
+    const requestBody = await request.json();
+    const registerStatus = await RegisterUser(requestBody.username, requestBody.password);
+    const response = new NextResponse;
+    
+    if (registerStatus)
+    {
+        response.headers.set("status", "200");
+    }
+    else
+    {
+        response.headers.set("status", "404");
+    }
+
+    return response;
+}
