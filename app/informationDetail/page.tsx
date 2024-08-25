40a687d29ca2fb6086ac38bdb73213882e4985d3
bry-ho <bryanho67@gmail.com>
2024-08-25 13:54:50 +0700

    feat: add static page

--- app/informationDetail/page.tsx
+++ app/informationDetail/page.tsx
@@ -1,64 +1,68 @@
-"use client"
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+"use client";
+import {useState} from "react";
+import HeaderWithBackButton from "../components/HeaderWithBackButton";
+import Navbar from "../components/Navbar";
 
 const DetailPage = () => {
-  // const router = useRouter();
-  // const { id } = router.query;
-  // const [data, setData] = useState(null);
+  const [isClicked, setIsClicked] = useState(false);
 
-  // useEffect(() => {
-  //   if (id) {
-  //     // Fetch data from API
-  //     axios
-  //       .get(`/api/${id}`)
-  //       .then((res) => {
-  //         setData(res.data);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   }
-  // }, [id]);
-  // console.log(data);
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
 
   return (
-    // data && (
-      <div className="my-20">
-        <div className="grid grid-cols-2 mx-40 py-10 ">
-          <div>
-            {/* for image */}
-            <img className="max-w-full rounded-lg" 
-            src= "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp" 
-            alt="" />
+    <div className="my-20">
+      <Navbar />
+      <div className="mx-20">
+        <HeaderWithBackButton title="Detail Lomba/Beasiswa/Seminar" />
+      </div>
+      <div className="grid grid-cols-2 mx-40 py-10 gap-5">
+        <div>
+          <img
+            className="max-w-full rounded-2xl"
+            src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"
+            alt=""
+          />
+        </div>
+        <div className="p-4 items-center border border-solid border-gray-300 w-full rounded-2xl shadow-custom">
+          <h1 className="mt-2 mb-2 text-3xl text-blue-800 tracking-tight font-extrabold">
+            Nama Lomba/Beasiswa/Seminar
+          </h1>
+          <hr className="mb-2 "></hr>
+          <div className="flex justify-end">
+            <button
+              className={`flex h-10 text-gray-800 dark:text-white hover:text-yellow-500 ${
+                isClicked ? "text-yellow-500" : ""
+              }`}
+              onClick={handleClick}
+            >
+              <svg
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width={30}
+                height={30}
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="m17 21-5-4-5 4V3.889a.92.92 0 0 1 .244-.629.808.808 0 0 1 .59-.26h8.333a.81.81 0 0 1 .589.26.92.92 0 0 1 .244.63V21Z"
+                />
+              </svg>
+              <span className="mt-0.5">Simpan</span>
+            </button>
           </div>
-          <div className="my-auto mx-10 items-center border border-solid border-gray-300 rounded-2xl shadow-custom">
-          <h1 className="mt-auto mb-4 text-4xl text-blue-800 tracking-tight font-extrabold">
-              {/* for title */} tes
-              {/* {data.title} */}
-            </h1>
-            <p className="mb-4 text-2xl  bold text-justify">
-              {/* for 
-              {data} */}hi
-            </p>
 
-            <p className="mb-4 bold text-justify">
-              {/* for caption */}gacor  
-              {/* {data.caption} */}
-              </p>
-            <p className="mb-4 bold text-justify">
-              {/* for post link */}
-              {/* {data} */}
-              </p>
-            <p className="mb-4 bold text-justify">
-              {/* Qualification: {data} */}
-              </p>
-          </div>
+          <p className="mb-2 text-xl  bold text-justify">Tanggal Posting:</p>
+          <p className="mb-2 text-xl bold text-justify">Penyelenggara:</p>
+          <p className="mb-2 text-xl bold text-justify">Detail:</p>
         </div>
       </div>
-    )
-  ;
+    </div>
+  );
 };
 
 export default DetailPage;
