b664900d5aede333a32fe8639accde438c99adff
Arlow5761 <arlow5761@gmail.com>
2024-08-25 14:17:30 +0700

    fix: fix sql syntax for getlomba

--- app/lib/getlomba.ts
+++ app/lib/getlomba.ts
@@ -3,7 +3,7 @@
 import { sql } from "@vercel/postgres";
 
 export default async function GetLomba(lombaID : string) {
-    const data = await sql<{ id : string, title : string }>`SELECT id, title FROM lomba WHERE id='${lombaID}';`;
+    const data = await sql<{ id : string, title : string }>`SELECT id, title FROM lomba WHERE id=${lombaID};`;
 
     if (data.rowCount != 1) {
         return null;
