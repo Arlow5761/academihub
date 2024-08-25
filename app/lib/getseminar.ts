4ec4f90b494781e82b8383538fa46de29522937b
Arlow5761 <arlow5761@gmail.com>
2024-08-25 14:36:32 +0700

    refactor: add description to seminar query result

--- app/lib/getseminar.ts
+++ app/lib/getseminar.ts
@@ -3,7 +3,7 @@
 import { sql } from "@vercel/postgres";
 
 export default async function GetSeminar(seminarID : string) {
-    const data = await sql<{ id : string, title : string }>`SELECT id, title FROM seminar WHERE id='${seminarID}';`;
+    const data = await sql<{ id : string, title : string, description : string }>`SELECT id, title, description FROM seminar WHERE id=${seminarID};`;
 
     if (data.rowCount != 1) {
         return null;
