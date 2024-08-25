a44ea442661866ac48804d660f97af20a7c9a3f0
Arlow5761 <arlow5761@gmail.com>
2024-08-25 14:37:37 +0700

    refactor: add description to query result for beasiswa

--- app/lib/getbeasiswa.ts
+++ app/lib/getbeasiswa.ts
@@ -3,7 +3,7 @@
 import { sql } from "@vercel/postgres";
 
 export default async function GetBeasiswa(beasiswaID : string) {
-    const data = await sql<{ id : string, title : string }>`SELECT id, title FROM beasiswa WHERE id='${beasiswaID}';`;
+    const data = await sql<{ id : string, title : string, description : string }>`SELECT id, title, description FROM beasiswa WHERE id=${beasiswaID};`;
 
     if (data.rowCount != 1) {
         return null;
