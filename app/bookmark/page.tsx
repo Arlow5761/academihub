600b879bfe0cc0458e007d082e02357668115a68
KarRz <reza11781@gmail.com>
2024-08-26 10:40:32 +0700

    feat: add Bookmark Page

---
+++ app/bookmark/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import CardListSection from '../components/CardListSection' 
+import Navbar from '../components/Navbar'; 
+import Header from '../components/HeaderWithBackButton';
+import SearchBar from '../components/SearchBar';
+import ListBeasiswa from '../lib/listbeasiswa';
+
+
+const BookmarkPage: React.FC = async () => {
+  const dataBeasiswa = await ListBeasiswa(0, 10, "", "")
+
+  return (
+    <main className="flex min-h-screen flex-col bg-[#F6F5F5]">
+      <Navbar/>
+      <div className="container mx-auto px-12 py-20">
+        <Header title="Saved Posts" />
+        <SearchBar/>
+        <CardListSection list={dataBeasiswa.beasiswa} type="beasiswa" />
+      </div>
+    </main>
+  );
+};
+
+export default BookmarkPage;
