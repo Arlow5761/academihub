cdf99ebde7a69f20cd956879ab7f6febcaedf63c
bry-ho <bryanho67@gmail.com>
2024-08-27 17:59:57 +0700

    feat: add landing page

---
+++ app/landingPage/component/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-white bg-opacity-100 shadow-xl">
+      <div className="flex items-center justify-between mx-auto px-4 py-3">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-4xl text-[#1F3C88] font-semibold"
+        >
+          AcademiHub
+        </Link>
+        <div className=" flex space-x-4 ">
+          <button className="border-2 border-orange-500 bg-white text-orange-500 text-xl font-semibold py-2 px-10 rounded-full hover:bg-orange-500 hover:text-white">
+            <Link href={"/register"}>Sign Up</Link>
+          </button>
+          <button className="border-2 border-orange-500 bg-orange-500 text-xl font-semibold text-white py-2 px-10 rounded-full hover:bg-white hover:text-orange-500">
+            <Link href={"/login"}>Login</Link>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
